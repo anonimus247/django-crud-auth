d51564e5d26399f6d365065fd0f226112022d9a9
Ian <iandev2307@gmail.com>
2024-11-07 22:31:49 -0700

    Home-Auth-2

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='postgresql://postgresql:postgresql@localhost/postgres',
-#         conn_max_age=600
-#     )
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgresql:postgresql@localhost/postgres',
+        conn_max_age=600
+    )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
